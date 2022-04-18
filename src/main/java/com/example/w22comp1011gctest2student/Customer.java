0586278c8f8fd69b49f657febb585ad82aacf21f
Bibek Poudel <200455715@student.georgianc.on.ca>
2022-04-18 08:22:48 -0400

    question 1 complete

--- src/main/java/com/example/w22comp1011gctest2student/Customer.java
+++ src/main/java/com/example/w22comp1011gctest2student/Customer.java
@@ -1,5 +1,32 @@
 package com.example.w22comp1011gctest2student;
 
+import java.util.ArrayList;
+
 public class Customer {
 
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private ArrayList<Product> purchases;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public ArrayList<Product> getPurchases() {
+        return purchases;
+    }
 }
