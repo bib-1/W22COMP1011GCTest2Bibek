66a59f8522e98e4004a27c477f13608abd84cb49
Bibek Poudel <200455715@student.georgianc.on.ca>
2022-04-18 08:29:27 -0400

    question 2 complete

--- src/main/java/com/example/w22comp1011gctest2student/Product.java
+++ src/main/java/com/example/w22comp1011gctest2student/Product.java
@@ -1,4 +1,34 @@
 package com.example.w22comp1011gctest2student;
 
 public class Product {
+  private String sku;
+  private String name;
+  private double salePrice;
+  private double regularPrice;
+  private String image;
+
+  public String getSku() {
+    return sku;
   }
+
+  public String getName() {
+    return name;
+  }
+
+  public double getSalePrice() {
+    return salePrice;
+  }
+
+  public double getRegularPrice() {
+    return regularPrice;
+  }
+
+  public String getImage() {
+    return image;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("%s-$%.2f",name, salePrice);
+  }
+}
